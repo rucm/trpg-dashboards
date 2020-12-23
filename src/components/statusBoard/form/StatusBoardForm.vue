6c8d116203cef9808a7cf677d2a6aa1a338a9633
rucm <rucm.info@gmail.com>
2020-12-23 22:30:21 +0900

    コンポーネント名を修正

--- src/components/statusBoard/form/StatusBoardForm.vue
+++ src/components/statusBoard/form/StatusBoardForm.vue
@@ -1,24 +1,24 @@
 <template>
   <status-board-wrap-box>
     <status-board-form-search-field></status-board-form-search-field>
-    <status-board-form-button></status-board-form-button>
-    <status-board-form-link></status-board-form-link>
+    <status-board-form-enter-button></status-board-form-enter-button>
+    <status-board-form-create-button></status-board-form-create-button>
   </status-board-wrap-box>
 </template>
 <script lang="ts">
 import { defineComponent } from '@vue/composition-api';
 import StatusBoardWrapBox from '@/components/statusBoard/common/StatusBoardWrapBox.vue';
 import StatusBoardFormSearchField from './StatusBoardFormSearchField.vue';
-import StatusBoardFormButton from './StatusBoardFormButton.vue';
-import StatusBoardFormLink from './StatusBoardFormLink.vue';
+import StatusBoardFormEnterButton from './StatusBoardFormEnterButton.vue';
+import StatusBoardFormCreateButton from './StatusBoardFormCreateButton.vue';
 
 export default defineComponent({
 
   components: { 
     StatusBoardWrapBox,
     StatusBoardFormSearchField,
-    StatusBoardFormButton, 
-    StatusBoardFormLink
+    StatusBoardFormEnterButton,
+    StatusBoardFormCreateButton
   }
 });
 </script>
