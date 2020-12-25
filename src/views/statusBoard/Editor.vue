c9d88385be82a11d2e0f2f7c6d8e026956e5a630
rucm <rucm.info@gmail.com>
2020-12-25 22:07:06 +0900

    ツールバーの実装

--- src/views/statusBoard/Editor.vue
+++ src/views/statusBoard/Editor.vue
@@ -1,5 +1,6 @@
 <template>
   <status-board-container>
+    <status-board-tool-bar></status-board-tool-bar>
     <status-board-card-group
       v-for="(group, index) in state.groups"
       :key="index"
@@ -10,12 +11,13 @@
 <script lang="ts">
 import { defineComponent, provide, SetupContext, onMounted, onBeforeUnmount } from '@vue/composition-api';
 import StatusBoardContainer from '@/components/statusBoard/common/StatusBoardContainer.vue';
+import StatusBoardToolBar from '@/components/statusBoard/editor/StatusBoardToolBar.vue';
 import StatusBoardCardGroup from '@/components/statusBoard/editor/StatusBoardCardGroup.vue';
 import { useStatusBoardModule, StatusBoardModuleKey } from '@/modules/statusBoard/editor';
 
 export default defineComponent({
 
-  components: { StatusBoardContainer, StatusBoardCardGroup },
+  components: { StatusBoardContainer, StatusBoardToolBar, StatusBoardCardGroup },
 
   setup (props: {}, ctx: SetupContext) {
     const statusBoardModule = useStatusBoardModule();
