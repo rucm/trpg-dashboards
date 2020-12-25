a0da40c9476c6857db752252c55d3d73208c7892
rucm <rucm.info@gmail.com>
2020-12-25 22:06:54 +0900

    CardGroupの編集ダイアログの実装

--- src/components/statusBoard/editor/StatusBoardCardGroup.vue
+++ src/components/statusBoard/editor/StatusBoardCardGroup.vue
@@ -1,12 +1,20 @@
 <template>
   <status-board-wrap-box>
     <v-card>
-      <v-card-title>{{ cardGroup.name }}</v-card-title>
+      <v-card-title>
+        <span>{{ cardGroup.name }}</span>
+        <v-spacer></v-spacer>
+        <v-btn icon @click="localState.dialog = true"><v-icon>mdi-pencil</v-icon></v-btn>
+        <status-board-card-group-dialog
+          v-model="localState.dialog"
+          :cardGroupIndex="cardGroupIndex"
+        ></status-board-card-group-dialog>
+      </v-card-title>
       <v-card-text>
         <status-board-card
           v-for="(card, index) in cardGroup.cards"
           :key="index"
-          :enableDisplayName="enableDisplayName"
+          :enableDisplayName="localState.enableDisplayCardName"
           :cardGroupIndex="cardGroupIndex"
           :cardIndex="index"
         ></status-board-card>
@@ -15,28 +23,32 @@
   </status-board-wrap-box>
 </template>
 <script lang="ts">
-import { defineComponent, inject, PropType, computed } from '@vue/composition-api';
+import { defineComponent, inject, PropType, computed, reactive } from '@vue/composition-api';
 import StatusBoardWrapBox from '@/components/statusBoard/common/StatusBoardWrapBox.vue';
+import StatusBoardCardGroupDialog from './dialog/StatusBoardCardGroupDialog.vue';
 import StatusBoardCard from './StatusBoardCard.vue';
 import { StatusBoardModuleKey, StatusBoardModule } from '@/modules/statusBoard/editor';
 
 export default defineComponent({
 
-  components: { StatusBoardWrapBox, StatusBoardCard },
+  components: { StatusBoardWrapBox, StatusBoardCard, StatusBoardCardGroupDialog },
 
   props: {
     cardGroupIndex: { type: Number as PropType<number>, required: true }
   },
 
   setup (props) {
-    const { state } = inject(StatusBoardModuleKey) as StatusBoardModule;
-    const cardGroup = state.groups[props.cardGroupIndex];
+    const { getCardGroup } = inject(StatusBoardModuleKey) as StatusBoardModule;
+    const cardGroup = computed(() => getCardGroup(props.cardGroupIndex));
 
-    const enableDisplayName = computed(() => (cardGroup.cards.length > 1));
+    const localState = reactive({
+      enableDisplayCardName: computed(() => (cardGroup.value.cards.length > 1)),
+      dialog: false
+    });
 
     return {
       cardGroup,
-      enableDisplayName
+      localState
     };
   }
 });
