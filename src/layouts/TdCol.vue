7a7b48a6c92bc71e8894087ae5b63de81126fbd4
rucm <rucm.info@gmail.com>
2021-02-10 00:27:04 +0900

    ベースレイアウトの追加

---
+++ src/layouts/TdCol.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-col :md="md" :sm="sm">
+    <slot></slot>
+  </v-col>
+</template>
+<script lang="ts">
+import { PropType, defineComponent } from '@vue/composition-api';
+
+export default defineComponent({
+
+  props: {
+    sm: { type: Number as PropType<number>, required: false, default: 10 },
+    md: { type: Number as PropType<number>, required: false, default: 8 }
+  }
+});
+</script>
---
+++ src/layouts/TdRow.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-row :justify="justify">
+    <slot></slot>
+  </v-row>
+</template>
+<script lang="ts">
+import { defineComponent, PropType } from '@vue/composition-api';
+
+export default defineComponent({
+  
+  props: {
+    justify: { type: String as PropType<string>, required: false, default: 'center' }
+  }
+});
+
+</script>
